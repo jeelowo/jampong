541c5b5aa04ee4f78852e6f26ed5b2786981ea48
jeelowo <plegariajamuel47@gmail.com>
2026-08-08 23:20:56 +0800

    running state now works properly

--- Scripts/Knight.gd
+++ Scripts/Knight.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 class_name Knight
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
 func _ready() -> void:
-	add_to_group("Knight")
+	add_to_group("Player")
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
--- Scripts/Player_Running.gd
+++ Scripts/Player_Running.gd
@@ -3,6 +3,9 @@ class_name PlayerRunning
 
 @onready var animation_player: AnimatedSprite2D = $"../../AnimationPlayer"
 
+@export var speed = 100
+var direction : float
+
 var player : CharacterBody2D
 
 func Enter():
@@ -11,7 +14,13 @@ func Enter():
 func Physics_Update(delta: float):
 	animation_player.play("Run")
 	
+	direction = Input.get_axis("move_left", "move_right")
 	
+	player.velocity.x = player.SPEED * direction
 	
-	if Input.get_axis("move_left", "move_right") == 0:
+	if direction == 0.0:
 		Transitioned.emit(self, "Idle")
+	elif direction == 1:
+		animation_player.flip_h = false
+	elif direction == -1:
+		animation_player.flip_h = true
