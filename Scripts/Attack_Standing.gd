8d99fbf9cb0dd3830a09f73e73d38c80920d84da
jeelowo <plegariajamuel47@gmail.com>
2026-08-09 13:25:10 +0800

    feat: prolonged attack when attack is spammed

--- Scripts/Attack_Standing.gd
+++ Scripts/Attack_Standing.gd
@@ -1,6 +1,11 @@
 extends State
 class_name AttackStanding
 
+@onready var standing_collision: CollisionShape2D = $"../../Standing Collision"
+@onready var crouch_collision: CollisionShape2D = $"../../Crouch Collision"
+@onready var slide_collision: CollisionShape2D = $"../../Slide Collision"
+@onready var roll_collision: CollisionShape2D = $"../../Roll Collision"
+
 @onready var animation_player: AnimatedSprite2D = $"../../AnimationPlayer"
 @onready var timer: Timer = $"../Timer"
 
@@ -8,15 +13,22 @@ var player = CharacterBody2D
 var finished_attacking : bool
 
 func Enter():
+	standing_collision.disabled = false
+	crouch_collision.disabled = true
+	slide_collision.disabled = true
+	roll_collision.disabled = true	
+
 	player = get_tree().get_first_node_in_group("Player")
 	finished_attacking = false
-	timer.start(1.0)
+	timer.start(0.5)
 
 func Physics_Update(delta: float):
 	animation_player.play("Attack Right")
-	print(timer.time_left)
 	
-	if Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right") and timer.is_stopped():
+	if Input.is_action_just_pressed("attack") and timer.time_left <= 0.25:
+		timer.start(timer.time_left + 0.5)
+		
+	if (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")) and timer.is_stopped():
 		Transitioned.emit(self, "Run")
 
 	if finished_attacking:
